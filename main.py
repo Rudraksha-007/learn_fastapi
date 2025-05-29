6568bad6d783ec195453d9297831e8cb0df671cb
Rudrakhsa Dwivedi <146867502+Rudraksha-007@users.noreply.github.com>
2025-05-29 17:38:55 +0530

    implemented sort by route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app=FastAPI()
 
@@ -19,3 +19,24 @@ def about():
 def view():
     data=load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str=Path(...,description="Enter the ID of the patient",example="54")):
+    # load all the data
+    data=load_data()
+    for dic in data:
+        if int(patient_id) == dic["id"]:
+            return dic
+    raise HTTPException(status_code=404,detail="Patient Not found :-C")
+
+@app.get("/sort")
+def sort_patient(sort_by:str=Query(...,description="Sort on the basis of age,height"),
+                 order:str= Query('asc',description="Sort in which order")):
+    valid_fields=("height","age")
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f"Invalid field select form {valid_fields}")
+    if order not in ('asc','dsc'):
+        raise HTTPException(status_code=400,detail=f"Invalid sort by select form asc or dsc")
+    data=load_data()
+    s_data=sorted(data,key=lambda dic : dic[sort_by],reverse=True if order=='dsc' else False)
+    return s_data
--- temp.py
+++ temp.py
@@ -1,17 +1,23 @@
 from faker import Faker
+from main import load_data
 import json
 import random
+
 def generate_height_cm(min_cm=150, max_cm=200):
     return round(random.uniform(min_cm, max_cm), 1) 
 fake=Faker()
 patients=[]
-for _ in range(100):
+for i in range(100):
     patients.append({
+        "id":i,
         "name":fake.name(),
         "height":generate_height_cm(),
+        "age":random.randint(15,90),        
         "phone":fake.phone_number(),
         "address":fake.address(),
         "verdict":fake.word(ext_word_list=["flu", "covid", "cold"])
+
     })
+
 with open("patient.json","w") as file:
     json.dump(patients,file,indent=2)
