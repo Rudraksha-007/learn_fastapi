c4cbb25be67f5216bd8b06a2617e8abb305a17cc
Rudrakhsa Dwivedi <146867502+Rudraksha-007@users.noreply.github.com>
2025-05-31 15:29:12 +0530

    Added pydantic schemas

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator,model_validator,computed_field
+from typing import List,Dict,Optional,Annotated
 from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app=FastAPI()
 
+class Patient(BaseModel):
+    name:Annotated[str,Field(max_length=50,title="Name of the patient not a PK in the DB",description="used to describe the non PK attribute of name",examples=["Nitish"])]
+    age:int
+    linkedin_Url:AnyUrl
+    email:EmailStr
+    married:Annotated[bool, Field(default=False,description="is the patient married or not?")]
+    weight:Annotated[float,Field(strict=True,gt=0,le=120)]
+    allergies:Annotated[Optional[List[str]],Field(default=None,max_length=16)]
+    contact_detail:Dict[str,str]
+
+class Patient2(BaseModel):
+    name:str
+    age:int
+    linkedin_Url:AnyUrl
+    email:EmailStr
+    married:bool
+    weight:float
+    height:float
+    allergies:List[str]
+    contact_detail:Dict[str,str]
+    
+    @computed_field
+    @property
+    def BMI(self)->float:
+        # this makes a attribute called "BMI" in the pydantic model
+        return round((self.weight/self.height**2),2)
+        
+
+    @field_validator('email')
+    @classmethod
+    def valid(cls,value):
+        valid_domains=('hdfc','icici')
+        domain_name=value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError("cannot treat this patient")
+        else:
+            return value
+    @field_validator('name',mode="after")
+    # this mode specifies when does field validator gets the value (before or after type coreation)
+    @classmethod
+    def transform_name(cls,name):
+        return name.upper()
+    
+    @model_validator(mode="after")
+    def validate_emergency_Contact(cls,model):
+        if model.age>=60 and "emergencyph" not in model.contact_detail.keys():
+            raise ValueError("Patient older than 60 must have a emergency ph")
+        else:
+            return model
 
 def load_data():
     with open("patient.json",'r')as file:
@@ -40,3 +91,4 @@ def sort_patient(sort_by:str=Query(...,description="Sort on the basis of age,hei
     data=load_data()
     s_data=sorted(data,key=lambda dic : dic[sort_by],reverse=True if order=='dsc' else False)
     return s_data
+
